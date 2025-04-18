df1ee7ccda097edbacf72d31c15d894abf5b23aa
Mateus Sfeir <mateussfeir@hotmail.com>
2025-04-18 16:17:46 -0400

    updated the dark theme of BIO and PROJECTS

--- config/urls.py
+++ config/urls.py
@@ -1,7 +1,13 @@
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
+import os
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('portfolio.urls')),  # Include portfolio app's URLs
 ]
+
+# Serve static files during development
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
