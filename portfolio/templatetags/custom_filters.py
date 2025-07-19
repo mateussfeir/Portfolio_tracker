d64a1055ac827766f7d8268625e63124960baba2
Mateus Sfeir <mateussfeir@hotmail.com>
2025-07-19 02:18:25 -0400

    Fixed the amount formating

---
+++ portfolio/templatetags/custom_filters.py
@@ -0,0 +1,11 @@
+from django import template
+
+register = template.Library()
+
+@register.filter
+def intcomma2(value, decimal_places=2):
+    try:
+        value = float(value)
+        return f"{value:,.{decimal_places}f}"
+    except (ValueError, TypeError):
+        return value
