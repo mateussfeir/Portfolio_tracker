fc3389263c944fc87325aeb788ca8c3c3ada51e0
Mateus Sfeir <mateussfeir@hotmail.com>
2025-07-23 16:58:10 -0400

    Add APScheduler to run daily net worth snapshot automatically

--- portfolio/apps.py
+++ portfolio/apps.py
@@ -4,3 +4,21 @@ from django.apps import AppConfig
 class PortfolioConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
     name = "portfolio"
+
+    def ready(self):
+        from apscheduler.schedulers.background import BackgroundScheduler
+        from django.core.management import call_command
+        import atexit
+        import os
+        # Prevent scheduler from running multiple times in development (runserver reload)
+        if os.environ.get('RUN_MAIN', None) != 'true':
+            return
+        scheduler = BackgroundScheduler()
+        scheduler.add_job(
+            lambda: call_command('daily_networth_snapshot'),
+            'interval',
+            days=1,
+            next_run_time=None  # avoids running immediately on startup
+        )
+        scheduler.start()
+        atexit.register(lambda: scheduler.shutdown())
