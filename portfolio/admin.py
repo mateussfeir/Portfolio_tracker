ab03bfe7a1d7656724ce3f123989ea509706a802
Mateus Sfeir <mateussfeir@hotmail.com>
2025-07-24 01:17:33 -0400

    Edit the admin page to add manually data

--- portfolio/admin.py
+++ portfolio/admin.py
@@ -2,4 +2,9 @@ from django.contrib import admin
 from .models import Asset, NetWorthSnapshot
 
 admin.site.register(Asset)
-admin.site.register(NetWorthSnapshot)
+
+class NetWorthSnapshotAdmin(admin.ModelAdmin):
+    list_display = ('user', 'net_worth', 'date', 'created_at')
+    fields = ('user', 'net_worth', 'date')
+
+admin.site.register(NetWorthSnapshot, NetWorthSnapshotAdmin)
--- portfolio/models.py
+++ portfolio/models.py
@@ -21,7 +21,7 @@ class Asset(models.Model):
 
 class NetWorthSnapshot(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
-    date = models.DateField(auto_now_add=True)
+    date = models.DateField()
     net_worth = models.DecimalField(max_digits=20, decimal_places=2)
     created_at = models.DateTimeField(auto_now_add=True)
 
