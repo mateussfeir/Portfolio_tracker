e56212de40c526cf5dd23797b0ecb7b890acd264
Mateus Sfeir <mateussfeir@hotmail.com>
2026-01-09 15:32:24 -0500

    fixing the static file duplicated problem

--- config/settings.py
+++ config/settings.py
@@ -110,10 +110,6 @@ USE_TZ = True
 
 STATIC_URL = "/static/"
 
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'portfolio', 'static'),
-]
-
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 
