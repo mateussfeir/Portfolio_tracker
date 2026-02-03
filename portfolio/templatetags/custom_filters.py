b4bad053a519ec78d0d3a07745181285c8b86b00
Mateus Sfeir <mateussfeir@hotmail.com>
2026-02-03 14:15:51 -0500

    Crypto tab shows 6 decimals now

--- portfolio/templatetags/custom_filters.py
+++ portfolio/templatetags/custom_filters.py
@@ -1,4 +1,5 @@
 from django import template
+from decimal import Decimal, InvalidOperation
 
 register = template.Library()
 
@@ -9,6 +10,15 @@ def intcomma2(value, decimal_places=2):
         return f"{value:,.{decimal_places}f}"
     except (ValueError, TypeError):
         return value
+
+@register.filter
+def crypto_amount(value, decimal_places=6):
+    try:
+        decimal_value = Decimal(str(value))
+    except (InvalidOperation, ValueError, TypeError):
+        return value
+
+    return format(decimal_value, f".{decimal_places}f")
 
 @register.filter
 def index(List, i):
